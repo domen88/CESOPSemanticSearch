57ef3c9791bb05e1cca5f4411e5d49510733c856
Domenico Scotece <domen.88@live.it>
2017-09-27 13:02:54 +0200

    demo program
    -jade and css for ui
    -java code for searching document

--- src/main/java/it/unibo/cesop/demo/Programma.java
+++ src/main/java/it/unibo/cesop/demo/Programma.java
@@ -1,16 +1,27 @@
 package it.unibo.cesop.demo;
 
+import it.unibo.cesop.demo.model.Document;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.template.jade.JadeTemplateEngine;
-
+import java.net.MalformedURLException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
 
 public class Programma {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws MalformedURLException {
 
         //Set Java Spark server port
         port(8080);
@@ -19,7 +30,8 @@ public class Programma {
         staticFileLocation("/public/css");
 
         //URL Solr
-        final String url = "http://localhost:8983/solr";
+        final SolrClient server = new HttpSolrClient.Builder("http://137.204.57.106:8983/solr/VisionTestCurriculumCollection").build();
+
 
         get("/", (request, response) -> {
             Map<String, String> model = new HashMap<>();
@@ -27,16 +39,31 @@ public class Programma {
             return new ModelAndView(model, "index"); // located in resources/templates directory
         }, new JadeTemplateEngine());
 
-        post("/cerca", (request, response) -> {
+        post("/cerca", (Request request, Response response) -> {
             //Start time
-            float startTime = System.currentTimeMillis();
+            Instant start = Instant.now();
+            Map<String, Object> model = new HashMap<>();
 
             String cerca = request.queryParams("cerca");
-            Map<String, String> model = new HashMap<>();
             model.put("message", cerca);
-            float endTime = System.currentTimeMillis();
-            float total = (endTime - startTime) / 1000;
-            model.put("total", String.valueOf(total));
+
+            //Set solr query
+            final String wildCard= "http\\:\\/\\/www.cesop.it\\/ontologies\\/";
+            SolrQuery query = new SolrQuery();
+            String squery = String.format("Competenze_ancestors_labels_t:%s OR Competenze_parent_labels_t:%s OR Competenze_label_t:%s", cerca, cerca, cerca);
+            query.setQuery(squery);
+            //query.addFilterQuery("Cognome:"+ cerca);
+
+            //Get results
+            QueryResponse rsp = server.query( query );
+            SolrDocumentList docs = rsp.getResults();
+            List<Document> beans = rsp.getBeans(Document.class);
+
+            Instant stop = Instant.now();
+            Duration duration = Duration.between(start, stop);
+            double seconds = (double)duration.getNano() / 1000000000.0;
+            model.put("total", String.format("%.3f", seconds));
+            model.put("documents", beans);
 
             return new ModelAndView(model, "cerca"); // located in resources/templates directory
         }, new JadeTemplateEngine());
